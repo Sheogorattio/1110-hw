4dc34ac185b3fe437f586397730d3db90ac0bf96
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-10-12 21:55:40 +0300

    Add project files.

---
+++ 1110 hw/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1110_hw
+{
+    class Calculator :INotifyPropertyChanged
+    {
+        double _a_value;
+        double _b_value;
+        double _result =0 ;
+        public double A_value
+        {
+            get
+            {
+                return _a_value;
+            }
+            set
+            {
+                _a_value = value;
+                _result = _a_value + _b_value;
+                onPropertyChanged(nameof(A_value));
+                onPropertyChanged(nameof(Result));
+            }
+        }
+        public double B_value
+        {
+            get
+            {
+                return _b_value;
+            }
+            set
+            { 
+                _b_value = value;
+                _result = _a_value + _b_value;
+                onPropertyChanged(nameof(B_value));
+                onPropertyChanged(nameof(Result));
+            }
+        }
+        public double Result
+        {
+            get
+            {
+                return _result;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void onPropertyChanged(params string[] Name)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            if (handler != null)
+            {
+                foreach (var name in Name)
+                {
+                    handler(this, new PropertyChangedEventArgs(name));
+                }
+            }
+        }
+    }
+  
+}
---
+++ 1110 hw/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1110_hw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+            this.DataContext = calc;
+        }
+    }
+}
